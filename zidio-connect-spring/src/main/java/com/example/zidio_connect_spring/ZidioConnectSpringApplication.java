464ef043926a0a13716d31df1a63ea4214dbd569
Abdul razak <abdulrazak.nasriudeen@gmail.com>
2025-06-30 13:26:07 +0530

    Spring boot setup completed

---
+++ zidio-connect-spring/src/main/java/com/example/zidio_connect_spring/ZidioConnectSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.zidio_connect_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZidioConnectSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZidioConnectSpringApplication.class, args);
+	}
+
+}
---
+++ zidio-connect-spring/src/test/java/com/example/zidio_connect_spring/ZidioConnectSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zidio_connect_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZidioConnectSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
